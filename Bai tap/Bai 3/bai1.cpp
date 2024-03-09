af1040d44a99979608ceea119f769aef6865b448
maianhpham123 <23021469@vnu.edu.vn>
2024-03-09 21:24:12 +0700

    added changes to repository

--- Bai tap/Bai 3/bai1.cpp
+++ Bai tap/Bai 3/bai1.cpp
@@ -1,9 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct point {
+    int x, y;
+};
+
+void print(point p) {
+    cout << p.x << " " << p.y;
+}
+
 int main()
 {
-  int n;
-  cin >> n;
-  cout << n<< endl;
+    point p;
+    p.x = 10;
+    p.y = 12;
+    print(p);
 }
