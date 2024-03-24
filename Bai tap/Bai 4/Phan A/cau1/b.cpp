4dc835835b0b5a94a85b53e3e5332c421b119e1a
Phạm Mai Anh <151472238+maianhpham123@users.noreply.github.com>
2024-03-24 15:26:45 +0700

    Create b.cpp

---
+++ Bai tap/Bai 4/Phan A/cau1/b.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+/*
+int arr1[3] = {1,2,3,4};
+
+int main() {
+    // Khai báo mảng trong hàm main
+    int arr2[2] = {1,2,3,4};
+
+    for (int i = 0; i < 4; i++) {
+        cout << arr1[i] << " ";
+    }
+    
+    cout << endl;
+
+    for (int i = 0; i < 4; i++) {
+        cout << arr2[i] << " ";
+    }
+    
+    return 0;
+}
+*/
+
+int arr1[5] = {1,2,3,4};
+
+int main() {
+    // Khai báo mảng trong hàm main
+    int arr2[6] = {1,2,3,4};
+
+    for (int i = 0; i < 5; i++) {
+        cout << arr1[i] << " ";
+    }
+    
+    cout << endl;
+
+    for (int i = 0; i < 6; i++) {
+        cout << arr2[i] << " ";
+    }
+    
+    return 0;
+}
