bd2a2891e5211bab7c9761cec91184a0b5d3e1a8
Phạm Mai Anh <151472238+maianhpham123@users.noreply.github.com>
2024-03-24 16:38:54 +0700

    Create b.cpp

---
+++ Bai tap/Bai 4/Phan A/cau 2/b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/* char str1 [3] = "abcd";
+int main()
+{
+  char str2 [2] = "abcd";
+  
+  for (int i = 0; i < 4; i++) cout << str1[i] << " ";
+  cout << endl;
+  for (int i = 0; i < 4; i ++) cout << str2[i] << " ";
+} */
+
+char str1 [5] = "abcd";
+int main()
+{
+  char str2 [6] = "abcd";
+  
+  for (int i = 0; i < 5; i++) cout << str1[i] << " ";
+  cout << endl;
+  for (int i = 0; i < 6; i ++) cout << str2[i] << " ";
+}
